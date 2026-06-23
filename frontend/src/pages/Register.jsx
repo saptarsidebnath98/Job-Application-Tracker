5595d721a62935e2d9fc881810b07cc541b7a7d5
Saptarsi Debnath <saptarsidebnath666666@gmail.com>
2026-06-23 23:46:02 +0530

    feat: basic register page setup - ui

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { Link } from "react-router-dom";
-import { validateEmail } from "../utils/utility";
+import { validateEmail, validatePassword } from "../utils/utility";
 
 
 const Register = () => {
@@ -8,6 +8,7 @@ const Register = () => {
     const [registerName, setRegisterName] = useState("");
     const [registerEmail, setRegisterEmail] = useState("");
     const [registerPassword, setRegisterPassword] = useState("");
+    const [error, setError] = useState({});
 
 
     const handleChange = (e, setFunc) => {
@@ -17,12 +18,37 @@ const Register = () => {
 
 
     const handleRegister = () => {
-        if(registerName && registerEmail && registerPassword && validateEmail(registerEmail)){
-            console.log({name: registerEmail, email: registerEmail, password: registerPassword})
+        if(!registerName){
+            setError(prevState => {
+                return {...prevState, name: "Please provide name"}
+            })
+        }
+        if(!registerEmail){
+            setError(prevState => {
+                return {...prevState, email: "Please provide email"}
+            })
+        }
+        if(!registerPassword){
+            setError(prevState => {
+                return {...prevState, password: "Please provide password"}
+            })
+        }
+        if(registerPassword && !validatePassword(registerEmail)){
+            setError(prevState => {
+                return {...prevState, password: "Password length must be 8 or above"}
+            })
+        }
+       
+
+        if(registerName && registerEmail && registerPassword && validateEmail(registerEmail) && validatePassword(registerEmail)){
+            console.log({name: registerName, email: registerEmail, password: registerPassword});
             setRegisterName("");
             setRegisterEmail("");
             setRegisterPassword("");
+            setError({});
         }
+
+        return;
     }
     return (
         <div>
@@ -34,10 +60,13 @@ const Register = () => {
                 <section id="register_form">
                     <label htmlFor="register_name">Name:</label>
                     <input type="text" id="register_name" placeholder="name" value={registerName} onChange={(e) => handleChange(e, setRegisterName)}/>
+                    <span className="error_text">{error.name && error.name}</span>
                     <label htmlFor="register_email">Email:</label>
                     <input type="text" id="register_email" placeholder="email" value={registerEmail} onChange={(e) => handleChange(e, setRegisterEmail)}/>
+                    <span className="error_text">{error.email && error.email}</span>
                     <label htmlFor="register_password">Password:</label>
                     <input type="password" id="register_password" placeholder="password" value={registerPassword} onChange={(e) => handleChange(e, setRegisterPassword)}/>
+                    <span className="error_text">{error.password && error.password}</span>
                     <div className="register_login_message">already have account?
                         <span>
                             <Link to="/login">Login</Link>
--- frontend/src/utils/utility.js
+++ frontend/src/utils/utility.js
@@ -2,3 +2,7 @@ export const validateEmail = (email) => {
     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return regex.test(email);
 }
+
+export const validatePassword = (password) => {
+    return password.length >= 8;
+}
